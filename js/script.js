a44bdf4c48d49e058c80d21776fd900f32953e1b
Stef Vercaemer <stefaan.vercaemer@howest.be>
2020-03-24 10:47:30 +0100

    Add css and instructions

--- js/script.js
+++ js/script.js
@@ -21,5 +21,20 @@ const KoppelEvents = () => {
 }
 
 const VulStandaardwaarden = () =>  {
-
+  
 }
+
+/*
+De volgende oefening simuleert een lottotrekking, 
+waarbij de gebruiker de kans krijgt zijn winnende nummers in te geven 
+via de daartoe voorziene inputvelden.
+
+Na de keuze klik je op de knop trekking en zie je de lottocijfers, welke random worden gekozen. 
+De nummers die overeenkomen met je keuze krijgen een andere achtergrondkleur.
+
+Werk deze toepassing uit en zorg voor de correcte foutmeldingen wanneer :
+- De gebruiker geen geldige nummers ingeeft (tussen 0 en 44).
+- De gebruiker lege velden laat in zijn combinatie
+Zorg er tevens voor dat de getrokken lottocijfers verschillend zijn, 
+door hiervoor een correct algoritme te bedenken.
+*/
